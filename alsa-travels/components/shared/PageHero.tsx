6599d25f5e4527d49914153b0244c4fff21266b9
Muhammad Abass Mudasir <maxmcgarth207@gmail.com>
2026-09-16 12:20:35 +0100

    Update PageHero component to use full screen height

--- alsa-travels/components/shared/PageHero.tsx
+++ alsa-travels/components/shared/PageHero.tsx
@@ -24,7 +24,7 @@ export default function PageHero({
   bgClass = 'bg-navy',
 }: PageHeroProps) {
   return (
-    <section className="relative min-h-[60vh] flex items-center justify-center overflow-hidden">
+    <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background image or solid color */}
       {imageSrc ? (
         <Image
